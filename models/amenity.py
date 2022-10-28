4247315bbb03fef36e868e1c0ee4b1bdaaeffad9
Amoo89 <aonifadeolanrewaju@gmail.com>
2022-10-28 12:34:40 -0400

    new code

---
+++ models/amenity.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" holds class Amenity"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+class Amenity(BaseModel, Base):
+    """Representation of Amenity """
+    if models.storage_t == 'db':
+        _tablename_ = 'amenities'
+        name = Column(String(128) nullable=False)
+    else
+        name = ""
+
+    def_init_(self, args, *kwargs):
+        """initializes Amenity"""
+        super()._init_(args, *kwargs)
